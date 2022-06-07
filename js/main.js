58bf5b190dd1f9763dae93128ca8e9d598176a5a
RAFAEL OLIVEIRA DE SOUZA <22102417@CFP127ALUNO.LOCAL>
2022-06-07 11:26:00 -0300

    Assets do Site

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const menu = document.querySelector('.menu-burguer-container');
+var aboutme = document.getElementById('about-me')
+var experienceAboutme = document.getElementById('experience-about-me')
+var oqeufaco = document.getElementById('oqeufaco');
+var experience = document.getElementById('experience')
+var contentOqeufaco = document.getElementById('content-oqeufaco')
+var oqeufacoCard = document.getElementById('oqeufaco-card');
+var contentExperience = document.getElementById('content-experience')
+var experienceCard = document.getElementById('experience-card');
+
+function menuAction (){
+    menu.classList.toggle ('show')
+}
+menu.addEventListener('click', menuAction)
+
+function sobremimAction (){
+    aboutme.classList.toggle ('hide')
+    contentOqeufaco.classList.toggle ('show')
+}
+oqeufaco.addEventListener('click', sobremimAction)
+
+function oqeufacoAction (){
+    aboutme.classList.remove ('hide')
+    contentOqeufaco.classList.remove ('show')
+}
+
+contentOqeufaco.addEventListener('click', oqeufacoAction)
+
+function experienceAction (){
+    aboutme.classList.toggle ('hide')
+    contentExperience.classList.toggle ('show')
+}
+experience.addEventListener('click', experienceAction)
+
+function experiencehideAction (){
+    aboutme.classList.remove ('hide')
+    contentExperience.classList.remove ('show')
+}
+
+contentExperience.addEventListener('click', experiencehideAction)
